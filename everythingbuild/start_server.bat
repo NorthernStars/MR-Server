af319904fc6936688278faaded1d88c15e9df584
Hannes Eilers <mail@hanneseilers.de>
2016-10-14 11:16:11 +0200

    Updated .gitignore and added start scripts

---
+++ everythingbuild/start_server.bat
@@ -0,0 +1,1 @@
+java -jar mrserver-runnable.jar -sl scenarios\mrscenariofootball.jar -sc mrscenariofootball.core.ScenarioCore -sc_al -bp 3311:3310 -bp_ao
---
+++ everythingbuild/start_server.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -jar mrserver-runnable.jar -sl scenarios/mrscenariofootball.jar -sc mrscenariofootball.core.ScenarioCore -sc_al -bp 3311:3310 -bp_ao
